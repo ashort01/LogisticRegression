02846ebed39e1ce96f062192a6ba9240d90e2a45
Austin Short <austinshort1@gmail.com>
2017-11-09 19:56:37 -0700

    new csv and bug fixes

--- ProcessAudio.py
+++ ProcessAudio.py
@@ -31,7 +31,16 @@ def ProcessData():
             fileid = file.split(".")[1]
             ExtractTestingFeatures(data, samplerate, fileid)
     np.savetxt("examples.csv", train_x, delimiter=",")
-    np.savetxt("classes.csv", train_y, delimiter=",")
+    with open("classes.csv", 'w') as c:
+        i=0
+        for x in train_y:
+            i += 1
+            c.write(str(x))
+            if i < len(train_y):
+                c.write(",")
+    c.close()
+
+
     np.savetxt("testing.csv",test_x, delimiter=",")
     with open("testing_ids.csv", 'w') as f:
         ids = ",".join(test_y)
@@ -52,26 +61,32 @@ def ExtractFeatures(data,samplerate,genre,index):
     #ofb = sp.FractionalOctaveFilterbank(ssamplerate,4)
     #y, states = ofb.filter(data)
     #L = (10 * np.log10(np.sum(y * y, axis=0)))[:30]
+
+
     features = np.concatenate((fft_features, mfcc), axis=0)
     train_x.append(features)
+    #save the genre for later
     train_y.append(genre)
 
 def ExtractTestingFeatures(data,samplerate,id):
+    #extract the fft feature
     fft_features = fftFeatures(data)
     # extract mfcc feature
     mfcc = cepsFeatures(data,samplerate)
     features = np.concatenate((fft_features, mfcc), axis=0)
+    #append the features to a 2d array
     test_x.append(features)
+    #append the id to an array for later
     test_y.append(id)
 
 def fftFeatures(data):
-    #todo: normalize this
+    #returns the fft features of the data
     return np.abs(scipy.fft(data)[:1000])
 
 def cepsFeatures(data, samplerate):
+    #returns the mfcc features of the data
     ceps = sp.mfcc(data, samplerate)
     numberOfCeps = len(ceps)
-    #TODO: normalize this
     return np.mean(ceps[int(numberOfCeps * 1 / 10):int(numberOfCeps * 9 / 10)], axis=0)
 
 print("Processing data...")
