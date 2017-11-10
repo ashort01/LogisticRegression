8744b5a60ff43550797365092e952e93356fc79c
Danny987 <crazydrums987@gmail.com>
2017-11-09 17:47:04 -0700

    added sklearn logistic regression

--- ProcessAudio.py
+++ ProcessAudio.py
@@ -3,13 +3,19 @@ import numpy as np
 import scipy
 import soundfile as sf
 import speechpy as sp
-import pyfilterbank as pf
+from sklearn import linear_model
+from sklearn import metrics
 
 genres = {"blues":0,"classical":1,"country":2,
           "disco":3, "hiphop":4, "jazz":5,
           "metal":6,"pop":7, "reggae":8,
-          "rock":9}
+          "rock":9}	  
 
+train_x = []
+train_y = []
+test_x = []
+test_y = []
+		  
 def ProcessData():
     for genre in genres.keys():
         dir = os.path.join(os.getcwd(), 'data\\genres\\'+genre)
@@ -24,7 +30,12 @@ def ProcessData():
 def ExtractFeatures(data,samplerate,genre,index):
     #extract first 1000 fft featues
     fft_features = abs(scipy.fft(data)[:1000])
-
+    train_x.append(fft_features)
+    train_y.append(genre)
+    if index > 80:
+        test_x.append(fft_features)
+        test_y.append(genre)
+	
     #extract MFCC features
     ceps =sp.mfcc(data,samplerate)
     numberOfCeps = len(ceps)
@@ -36,5 +47,9 @@ def ExtractFeatures(data,samplerate,genre,index):
     L = (10 * np.log10(np.sum(y * y, axis=0)))[:30]
 
 
-
+print "Processing data..."
 ProcessData()
+print "Creating model..."
+mul_lr = linear_model.LogisticRegression(multi_class='multinomial',solver ='newton-cg').fit(train_x, train_y)
+print "Multinomial Logistic regression Train Accuracy :: ", metrics.accuracy_score(train_y, mul_lr.predict(train_x))
+print "Multinomial Logistic regression Train Accuracy :: ", metrics.accuracy_score(test_y, mul_lr.predict(test_x))
